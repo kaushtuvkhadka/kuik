de68aa4b945c5a87a98c3389cad5ca757688f0c6
kaushtuv khadka <kkcritix@gmail.com>
2026-08-02 18:09:28 +0545

    removed innitial app folder

--- app/.qt/KUik_res.cmake
+++
@@ -1,7 +0,0 @@
-
-set(target "KUik")
-set(working_dir "C:/Users/kaushtuv/Desktop/coding/qtprojects/kuik-main")
-set(src_and_dest_list
-
-)
-set(timestamp_file "C:/Users/kaushtuv/Desktop/coding/qtprojects/kuik-main/app/.qt/KUik_res.txt")
